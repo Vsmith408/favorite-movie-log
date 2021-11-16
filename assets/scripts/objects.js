2136844ec90c961f7234de32e2c289aad2fd242d
Victoria Smith <victoriasmithh428@gmail.com>
2021-11-16 16:45:17 -0500

    filter movies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -5,7 +5,7 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 // Render movies
-const renderMovies = () => {
+const renderMovies = (filter = "") => {
   const movieList = document.getElementById("movie-list");
 
   if (movies.length === 0) {
@@ -16,7 +16,11 @@ const renderMovies = () => {
   }
   movieList.innerHTML = "";
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
     let text = movie.info.title + " - ";
     for (const key in movie.info) {
@@ -55,4 +59,10 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
