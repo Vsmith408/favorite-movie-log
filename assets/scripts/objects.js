a19a20ff0c0989853db977fe64b170e54a73d9fe
Victoria Smith <victoriasmithh428@gmail.com>
2021-11-17 16:00:45 -0500

    destructuring

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -22,10 +22,15 @@ const renderMovies = (filter = "") => {
 
   filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
-    let text = movie.info.title + " - ";
-    for (const key in movie.info) {
+    // object destructuring
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    // object destructuring & changing name
+    const { title: movieTitle } = info;
+    let text = movieTitle + " - ";
+    for (const key in info) {
       if (key !== "title") {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -50,7 +55,7 @@ const addMovieHandler = () => {
   const newMovie = {
     info: {
       title,
-      [extraName]: extraName,
+      [extraName]: extraValue,
     },
     id: Math.random(),
   };
